46ff450afa78c5d309d344ce04e2c274d50fe3bf
AramMalkhasyan <138795933+AramMalkhasyan@users.noreply.github.com>
2025-11-15 01:15:15 +0400

    created and added profile complition progress bar and dinamic functionality

--- goalise/src/components/ProfileComplitionsProgresbar/ProfileComplitionsProgresbar.tsx
+++ goalise/src/components/ProfileComplitionsProgresbar/ProfileComplitionsProgresbar.tsx
@@ -1,18 +1,24 @@
 "use client";
 
+import { useGetUserInfoQuery } from "@/app/store/services/api";
 import styles from "./ProfileComplitionsProgresbar.module.css";
 
-type ProfileComplitionsProgresbarProps = {
-  value: number; // 0 - 100
-};
-
-export const ProfileComplitionsProgresbar = ({
-  value,
-}: ProfileComplitionsProgresbarProps) => {
-  const safeValue = Math.max(0, Math.min(100, value));
+export const ProfileComplitionsProgresbar = () => {
+  const { data: userInfo } = useGetUserInfoQuery();
+  console.log(userInfo, "user info in progres bar");
+  const safeValue = userInfo?.profileCompletionInfo?.percentage || 0;
+  let pointDistance = 0;
+  console.log(safeValue, "value");
 
   return (
     <div className={styles.container}>
+      <div className={styles.steps_name_wrapper}>
+        {userInfo?.profileCompletionInfo?.steps.map((step, index) => (
+          <div key={index} className={styles.step}>
+            <span>{step.name}</span>
+          </div>
+        ))}
+      </div>
       <div
         className={styles.track}
         role="progressbar"
@@ -21,26 +27,44 @@ export const ProfileComplitionsProgresbar = ({
         aria-valuenow={safeValue}
       >
         <div className={styles.fill} style={{ width: `${safeValue}%` }} />
-        <div
+        {userInfo?.profileCompletionInfo?.steps.map((step, index) => {
+          pointDistance += step.percentage;
+          console.log(pointDistance, "point distance");
+
+          return (
+            <div
+              key={index}
+              className={`${styles.circle} ${styles.circle_0} ${
+                safeValue >= 0 ? styles.active : ""
+              }`}
+              style={{ left: `${pointDistance}%` }}
+            />
+          );
+        })}
+        {/* <div
           className={`${styles.circle} ${styles.circle_0} ${
             safeValue >= 0 ? styles.active : ""
           }`}
+          style={{ left: "0%" }}
         />
         <div
           className={`${styles.circle} ${styles.circle_33} ${
             safeValue >= 33 ? styles.active : ""
           }`}
+          style={{ left: "33.33%" }}
         />
         <div
           className={`${styles.circle} ${styles.circle_66} ${
             safeValue >= 66 ? styles.active : ""
           }`}
+          style={{ left: "66.66%" }}
         />
         <div
           className={`${styles.circle} ${styles.circle_100} ${
             safeValue >= 100 ? styles.active : ""
           }`}
-        />
+          style={{ left: "100%" }}
+        /> */}
       </div>
     </div>
   );
